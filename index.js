e8ba4af342a59fc760559a9607e6ebc51dc7dd00
Dima Pasko <dpa@scalepoint.com>
2016-12-06 13:05:04 +0200

    Fix obsolete Gitbook plugin API usage Support gitbook 3.2.2 Fixes #1 https://github.com/ly-tools/gitbook-plugin-todo/issues/1

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ var re = /^\s*\[[x ]\]\s*/;
 
 module.exports = {
   hooks: {
-    page: function(page) {
-      _.each(page.sections, function(section) {
-        var $ = cheerio.load(section.content);
-        $('li').each(function(index, a) {
-          a = $(a);
-          var text = a.text();
-          if (re.test(text)) {
-            text = text
-              .replace(/^\s*\[ \]\s*/, '<input type="checkbox" disabled="disabled"></i> ')
-              .replace(/^\s*\[x\]\s*/, '<input type="checkbox" disabled="disabled" checked="checked"></i> ');
-            a.replaceWith('<li style="list-style: none">' + text + '</li>');
-          }
-        });
-        section.content = $.html();
+    page: function (page) {
+      var $ = cheerio.load(page.content);
+      $('li').each(function (index, a) {
+        a = $(a);
+        var text = a.text();
+        if (re.test(text)) {
+          text = text
+            .replace(/^\s*\[ \]\s*/, '<input type="checkbox" disabled="disabled"></i> ')
+            .replace(/^\s*\[x\]\s*/, '<input type="checkbox" disabled="disabled" checked="checked"></i> ');
+          a.replaceWith('<li style="list-style: none">' + text + '</li>');
+        }
       });
+      page.content = $.html();
+
       return page;
     }
   }
